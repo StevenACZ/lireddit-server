4d3f6ac81bf2868f2a9629216bccbefc81687504
stevenacz <scoailazaa@gmail.com>
2021-09-19 02:23:37 -0500

    🧰 comments added

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,21 +1,28 @@
+// MikroORM
 import { Entity, PrimaryKey, Property } from '@mikro-orm/core';
+
+// TypeGraphQL
 import { Field, Int, ObjectType } from 'type-graphql';
 
 @ObjectType()
 @Entity()
 export class Post {
+	// id
 	@Field(() => Int)
 	@PrimaryKey()
 	id!: number;
 
+	// createAt
 	@Field(() => Date)
 	@Property({ type: 'date' })
 	createdAt: Date = new Date();
 
+	// updateAt
 	@Field(() => Date)
 	@Property({ type: 'date', onUpdate: () => new Date() })
 	updatedAt: Date = new Date();
 
+	// title
 	@Field(() => String)
 	@Property({ type: 'text' })
 	title!: string;
