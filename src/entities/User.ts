58591a691eb2544ac37e638efcb83b577f2c5388
stevenacz <scoailazaa@gmail.com>
2021-09-19 02:23:26 -0500

    🧰 comments added

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,25 +1,33 @@
+// MikroORM
 import { Entity, PrimaryKey, Property } from '@mikro-orm/core';
+
+// TypeGraphQL
 import { Field, Int, ObjectType } from 'type-graphql';
 
 @ObjectType()
 @Entity()
 export class User {
+	// id
 	@Field(() => Int)
 	@PrimaryKey()
 	id!: number;
 
+	// createdAt
 	@Field(() => String)
 	@Property({ type: 'date' })
 	createdAt: Date = new Date();
 
+	// updateAt
 	@Field(() => String)
 	@Property({ type: 'date', onUpdate: () => new Date() })
 	updatedAt: Date = new Date();
 
+	// username
 	@Field(() => String)
 	@Property({ type: 'text', unique: true })
 	username!: string;
 
+	// password
 	@Property({ type: 'text' })
 	password!: string;
 }
